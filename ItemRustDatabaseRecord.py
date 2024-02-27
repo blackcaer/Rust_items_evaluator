2417539cbf005740fabd949a6799a02a9cc41ef0
blackcaer <kryspin123@onet.pl>
2024-02-27 22:31:04 +0100

    Implemented calc_expiry_date

--- ItemRustDatabaseRecord.py
+++ ItemRustDatabaseRecord.py
@@ -1,6 +1,4 @@
-import ItemRust
-import time
-
+from datetime import datetime, timedelta
 
 class ItemRustDatabaseRecord:
     """ Encapsulates data of ItemRust for ItemRustDatabase class"""
@@ -13,7 +11,7 @@ class ItemRustDatabaseRecord:
         self.price_sp = None
         self.pricehistory_sp = None
         self.sales_histogram_sp = None
-        self.timestamp = None
+        self.timestamp: datetime = None
 
         self.value = None
 
@@ -51,3 +49,52 @@ class ItemRustDatabaseRecord:
         itemrust.sales_histogram_sp = self.sales_histogram_sp
 
         itemrust.timestamp = self.timestamp
+
+    def calc_expiry_date(self, min_expiry_time=2, high_value=2.5, max_expiry_time=7, low_value=1.5,
+                         expire_on_friday=True):
+        """ Calculates item's data expiry date based on its value. Higher value = shorted expiration time.
+        :param min_expiry_time: Days to expiration for items with >= high_value
+        :type min_expiry_time: float
+        :param high_value: Items with >= values will have max_expiry_time days to expiration
+        :type high_value: float
+        :param max_expiry_time: Days to expiration for items with <= low_value
+        :type max_expiry_time:float
+        :param low_value: Items with <= values will have min_expiry_time days to expiration
+        :type low_value:float
+        :param expire_on_friday: If True, expires item on 00:00 of next Friday (itemstore update)
+        :type expire_on_friday:float
+        :return: Date of item's expiration
+        :rtype: datetime
+
+        """
+        if high_value < low_value:
+            raise ValueError("low_value cannot be greater than high_value")
+        if max_expiry_time<min_expiry_time:
+            raise ValueError("min_expiry_time cannot be greater than max_expiry_time")
+
+        val = self.value
+
+        if val <= low_value:
+            expiry_time_days = max_expiry_time
+        elif val >= high_value:
+            expiry_time_days = min_expiry_time
+        else:
+            # Calculate expiry time days by scaling time proportionally to value between low and high value
+            mtp = (val-low_value) / (high_value - low_value)
+            expiry_time_days = min_expiry_time + mtp * (max_expiry_time - min_expiry_time)
+
+        expiry_date = self.timestamp + timedelta(days=expiry_time_days)
+
+        if expire_on_friday:
+            day = self.timestamp + timedelta(days=1)
+            while day.weekday() != 4:
+                day += timedelta(days=1)
+
+            friday_expiration = day.replace(hour=0, minute=0, second=0, microsecond=0)
+
+            if expiry_date > friday_expiration:
+                expiry_date = friday_expiration
+
+        return expiry_date
+
+
